cd392990f55169fcee68241d089c56e7139539f9
katongole <katsisaac50@gmail.com>
2023-09-27 07:37:59 +0200

    setting different types

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 var sport = 'football';
 var id = 5;
+id = "5";
+// A tuple
+var person = ['Danny', 1, true];
+person[0] = 100;
+person[1] = { name: 'Danny' }; // Error - person array can't contain objects
+// union types
+var person = ['Danny', 1, true];
+person[0] = 100;
+person[1] = { name: 'Danny' }; // Error - person array can't contain objects
--- index.ts
+++ index.ts
@@ -1,3 +1,14 @@
 let sport = 'football';
 
-let id = 5;
+let id: number|string = 5;
+
+id ="5";
+// A tuple
+let person: (string | number | boolean)[] = ['Danny', 1, true];
+person[0] = 100;
+person[1] = {name: 'Danny'} // Error - person array can't contain objects
+
+// union types
+let person: (string | number | boolean)[] = ['Danny', 1, true];
+person[0] = 100;
+person[1] = {name: 'Danny'} // Error - person array can't contain objects
